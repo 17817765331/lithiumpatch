5d6c83647fcad60b1cc4dd0e727611d1c20a223c
17817765331 <63466566+17817765331@users.noreply.github.com>
2026-02-24 10:01:38 +0800

    Update extrafonts.go

--- patches/extrafonts.go
+++ patches/extrafonts.go
@@ -22,7 +22,20 @@ func (extrafonts) Do(apk string, diffwriter io.Writer) error {
 		return nil
 	}
 	var pt []Instruction
-	
+	for _, f := range xs {
+		if f.Regular != nil {
+			pt = append(pt, WriteFile("assets/fonts/"+f.Base+"-Regular.ttf", f.Regular))
+		}
+		if f.Bold != nil {
+			pt = append(pt, WriteFile("assets/fonts/"+f.Base+"-Bold.ttf", f.Bold))
+		}
+		if f.Italic != nil {
+			pt = append(pt, WriteFile("assets/fonts/"+f.Base+"-Italic.ttf", f.Italic))
+		}
+		if f.BoldItalic != nil {
+			pt = append(pt, WriteFile("assets/fonts/"+f.Base+"-BoldItalic.ttf", f.BoldItalic))
+		}
+	}
 	pt = append(pt, PatchFile("smali/com/faultexception/reader/fonts/Fonts.smali",
 		InMethod("<clinit>()V",
 			ReplaceStringAppend(
@@ -46,32 +59,27 @@ func (extrafonts) Do(apk string, diffwriter io.Writer) error {
 				.locals 7
 				{{range .}}
 				const-string v1, "{{.Name}}"
-				
 				{{if .Regular -}}
-				const-string v2, "/sdcard/Lithium/fonts/{{.Base}}-Regular.ttf"
+				const-string v2, "{{.Base}}-Regular.ttf"
 				{{- else -}}
 				const/4 v2, 0x0
 				{{- end}}
-				
 				{{if .Bold -}}
-				const-string v3, "/sdcard/Lithium/fonts/{{.Base}}-Bold.ttf"
+				const-string v3, "{{.Base}}-Bold.ttf"
 				{{- else -}}
 				const/4 v3, 0x0
 				{{- end}}
-				
 				{{if .Italic -}}
-				const-string v4, "/sdcard/Lithium/fonts/{{.Base}}-Italic.ttf"
+				const-string v4, "{{.Base}}-Italic.ttf"
 				{{- else -}}
 				const/4 v4, 0x0
 				{{- end}}
-				
 				{{if .BoldItalic -}}
-				const-string v5, "/sdcard/Lithium/fonts/{{.Base}}-BoldItalic.ttf"
+				const-string v5, "{{.Base}}-BoldItalic.ttf"
 				{{- else -}}
 				const/4 v5, 0x0
 				{{- end}}
-				
-				const/16 v6, {{.Script.Flags | printf "%#x"}}
+				const/16 v6, {{.Script.Flags | printf "%#x"}} # {{.Script}}
 
 				new-instance v0, Lcom/faultexception/reader/fonts/Font;
 				invoke-direct/range {v0 .. v6}, Lcom/faultexception/reader/fonts/Font;-><init>(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;I)V
@@ -90,7 +98,7 @@ func (extrafonts) Do(apk string, diffwriter io.Writer) error {
 					invoke-virtual {p0, v0}, Ljava/lang/String;->split(Ljava/lang/String;)[Ljava/lang/String;
 					move-result-object p0
 
-					const/4 v0, 0x0
+					const v0, 0
 					aget-object p0, p0, v0
 					invoke-virtual {p0}, Ljava/lang/String;->toLowerCase()Ljava/lang/String;
 					move-result-object p0
@@ -100,19 +108,14 @@ func (extrafonts) Do(apk string, diffwriter io.Writer) error {
 					const-string v0, "{{.}}"
 					invoke-virtual {p0, v0}, Ljava/lang/String;->equals(Ljava/lang/Object;)Z
 					move-result v0
-					if-eqz v0, :script_match
+					const/16 v4, {{$x.Script.Flags | printf "%#x"}} # {{$x.Script}}
+					if-nez v0, :filter
 					{{- end}}
 					{{- end}}
-					
-					const/16 v4, 0x0
-					goto :filter_start
 
-					{{range $x := .}}
-					:script_match_{{$x.Script}}
-					const/16 v4, {{$x.Script.Flags | printf "%#x"}}
-					{{end}}
+					const/16 v4, 0 # any (default)
 
-					:filter_start
+					:filter
 					new-instance v0, Ljava/util/ArrayList;
 					invoke-direct {v0}, Ljava/util/ArrayList;-><init>()V
 
@@ -121,10 +124,10 @@ func (extrafonts) Do(apk string, diffwriter io.Writer) error {
 					invoke-interface {v1}, Ljava/util/List;->iterator()Ljava/util/Iterator;
 					move-result-object v1
 
-					:cond_loop
+					:filter_next
 					invoke-interface {v1}, Ljava/util/Iterator;->hasNext()Z
 					move-result v2
-					if-nez v2, :return_result
+					if-eqz v2, :filter_done
 
 					invoke-interface {v1}, Ljava/util/Iterator;->next()Ljava/lang/Object;
 					move-result-object v2
@@ -132,12 +135,12 @@ func (extrafonts) Do(apk string, diffwriter io.Writer) error {
 
 					iget v3, v2, Lcom/faultexception/reader/fonts/Font;->scripts:I
 					and-int/2addr v3, v4
-					if-eq v3, v4, :cond_loop
+					if-ne v3, v4, :filter_next
 
 					invoke-interface {v0, v2}, Ljava/util/List;->add(Ljava/lang/Object;)Z
-					goto :cond_loop
-
-					:return_result
+					goto :filter_next
+				
+					:filter_done
 					return-object v0
 				`, []struct {
 					Script   fonts.Script
@@ -147,11 +150,11 @@ func (extrafonts) Do(apk string, diffwriter io.Writer) error {
 					{fonts.FontScriptCyrillic, []string{"rus", "ru"}},
 					{fonts.FontScriptGreek, []string{"gre", "ell", "el"}},
 					{fonts.FontScriptThai, []string{"tha", "th"}},
+					// default is any
 				})),
 			),
 		),
 	))
-	
 	for _, x := range pt {
 		if err := x.Do(apk, diffwriter); err != nil {
 			return err
